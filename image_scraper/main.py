5f1cdc8f9f2e3cc24deffd70f88945f91307d372
Vasanth2005kk <vasanth2005kk@gmail.com>
2024-09-14 08:10:31 +0530

    os module conected

--- image_scraper/main.py
+++ image_scraper/main.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from bs4 import BeautifulSoup
 
 url = "https://www.pinterest.com/scarlxrd18/anime-wallpaper/"
@@ -7,8 +8,12 @@ responce = requests.get(url).content
 soup = BeautifulSoup(responce , "html.parser")
 
 all_image_tags= soup.find_all('img', {"class":"hCL kVc L4E MIw"})
-
 image_count=0
+
+floder_name=input("enter the downloade floder name :".title())
+
+# create floder
+os.mkdir(f"{os.getcwd()}/image_scraper/{floder_name}") 
 
 for img in all_image_tags:
     image_url = img["src"]
@@ -18,7 +23,7 @@ for img in all_image_tags:
 
     image_count+=1
 
-    img_name = f"DOWNLODE_IMAGES/image_{image_count}.{jpg_formate}"
-
+    img_name = f"{os.getcwd()}/image_scraper/{floder_name}/image_{image_count}.{jpg_formate}"
+    
     with open(img_name , "wb") as file:
         file.write(responce)
